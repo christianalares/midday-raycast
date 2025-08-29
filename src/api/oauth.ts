972b4dc475444fd5c3d9f73a3c87f03d06b2094a
Christian Alares <christian.alares@gmail.com>
2025-08-29 22:08:52 +0200

    add back PKCE oauth flow

--- src/api/oauth.ts
+++ src/api/oauth.ts
@@ -16,15 +16,9 @@ export const middayOAuth = new OAuthService({
   client,
   clientId: "mid_client_ae0Ldc62GX117eirIjIZnqVD",
   scope: "apis.all",
-  authorizeUrl:
-    "https://oauth.raycast.com/v1/authorize/r53Ekwg57FYCh8iLGOc9MBxVFvVmntETGo4KUZDJ491b310MjV9KEAF47F8I_soC4dmys6UCAHKxq6XOWMBeI39Wf6cm6-Y5dCpYSGqUTSXcbnwc6CR8pREjcLR_Q76vZnTDfCDbBjGROx64s-ME85Uz",
-  tokenUrl:
-    "https://oauth.raycast.com/v1/token/0kK9tWDNMe4zoRZAXjhvg1GmDIJ7r6RUl1v44W1DcyGDWRxYIWn15bSkXt6dzPUvk595OBfNXf13SDXNbqedWQy2uVZptuHdR7ZoDnzvLIfVTdjnloGvEhqLf-CKuMPF7865cptY65Nmnr-Da1M",
-  refreshTokenUrl:
-    "https://oauth.raycast.com/v1/refresh-token/itxZG4UwXhufOr284n5bMRk1lGN1s1ka8lxQpu2Oi6BR6kA6attrMPrPyKsXrhvsb_FpPJUvP7TJSjz5GEOYtyM7o-mI_cLGw9PyDvBTfk1CCdrfEbpVCUXEGScx-AOY0bB_kn_4wCqwbyRBOjE",
-  // authorizeUrl: 'https://app.midday.ai/oauth/authorize',
-  // tokenUrl: 'https://api.midday.ai/oauth/token',
-  // refreshTokenUrl: 'https://api.midday.ai/oauth/token',
+  authorizeUrl: "https://app.midday.ai/oauth/authorize",
+  tokenUrl: "https://api.midday.ai/oauth/token",
+  refreshTokenUrl: "https://api.midday.ai/oauth/token",
   bodyEncoding: "json",
   onAuthorize({ token }) {
     midday = new Midday({
